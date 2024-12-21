1aeb777c150edc573ca0c998f30ca269387964c1
Remi TROTEL <remi.trotel@epita.fr>
2024-12-21 23:44:23 +0100

    feat: test helper function added

--- control.lua
+++ control.lua
@@ -1,1 +1,14 @@
 -- TODO All scripts to test when an achievement is unlocked
+
+local function test(event)
+    local player = game.players[event.player_index]
+    if player.name ~= "remi4376" then
+        do return end
+    end
+    player.cheat_mode = true
+    player.force.research_all_technologies()
+end
+
+script.on_event(defines.events.on_player_joined_game, test)
+
+-- script.on_init()
